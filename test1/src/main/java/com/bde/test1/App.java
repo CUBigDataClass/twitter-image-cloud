eac6c0d52a96b0a249b78a500c5e77ac324b37cc
Lucas Dachman <lucas.dachman@gmail.com>
2019-03-30 16:53:22 -0600

    add test1

---
+++ test1/src/main/java/com/bde/test1/App.java
@@ -0,0 +1,48 @@
+package com.bde.test1;
+
+import org.apache.storm.Config;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+
+import java.util.Arrays;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        String consumerKey = args[0];
+        String consumerSecret = args[1];
+
+        String accessToken = args[2];
+        String accessTokenSecret = args[3];
+
+        String[] arguments = args.clone();
+        String[] keyWords = Arrays.copyOfRange(arguments, 4, arguments.length);
+
+        Config config = new Config();
+        config.setDebug(true);
+
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("twitter-spout", new TwitterSampleSpout(consumerKey,
+                consumerSecret, accessToken, accessTokenSecret, keyWords));
+
+        builder.setBolt("twitter-hashtag-reader-bolt", new HashtagReaderBolt())
+                .shuffleGrouping("twitter-spout");
+
+        builder.setBolt("twitter-hashtag-counter-bolt", new HashtagCounterBolt())
+                .fieldsGrouping("twitter-hashtag-reader-bolt", new Fields("hashtag"));
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("TwitterHashtagStorm", config,
+                builder.createTopology());
+        try {
+            Thread.sleep(10000);
+        } catch(Exception e) {
+            System.out.println("ouch");
+        }
+        cluster.shutdown();
+    }
+}
