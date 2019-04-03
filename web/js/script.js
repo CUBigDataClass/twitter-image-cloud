873aaef337e4f1e5f16f8b07f94405e8dfd09d74
angr1252 <47996387+angr1252@users.noreply.github.com>
2019-04-03 17:25:14 -0600

    Add files via upload

--- web/js/script.js
+++ web/js/script.js
@@ -6,3 +6,53 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+//zingChart
+var myConfig = {
+"graphset":[
+{
+"type":"wordcloud",
+"options":{
+  "style":{
+    "tooltip":{
+      visible: true,
+      text: '%text: %hits'
+    },
+	fontFamily: 'Roboto'
+  },
+"words":[
+{
+"text":"Press",
+"count":"400"
+},
+{
+"text":"USA",
+"count":"300"
+},
+{
+"text":"Waffles",
+"count":"200"
+},
+{
+"text":"hello",
+"count":"150"
+},
+{
+"text":"morning",
+"count":"100"
+},
+{
+"text":"fox",
+"count":"20"
+}
+]
+}
+}
+]
+};
+ 
+zingchart.render({ 
+	id: 'myChart', 
+	data: myConfig, 
+	height: '100%', 
+	width: '100%' 
+});
