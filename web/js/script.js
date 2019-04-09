448a3dc503e70a79916dc7954c6a3c9a4f607175
angr1252 <47996387+angr1252@users.noreply.github.com>
2019-04-08 22:06:46 -0600

    Add files via upload

--- web/js/script.js
+++ web/js/script.js
@@ -1,51 +1,72 @@
+/* Variable declarations */
 var slider = document.getElementById("myRange");
 var output = document.getElementById("sValue");
 output.innerHTML = slider.value;
-
+/* AJAX Request Initialize */
+var ourRequest = new XMLHttpRequest();
+/* words array of objects */
+var words = [];
+/* Word cloud function */
+function wCloud() {
+    var myConfig = {
+        "graphset": [ 
+        {
+            "type": "wordcloud",  
+            "options": {
+            "style": {
+                "tooltip": {
+                visible: true,
+                text: '%text: %hits'
+                },
+                fontFamily: 'Roboto'
+            },
+            words
+            }
+        }
+        ]
+    };
+    zingchart.render({ 
+        id: 'myChart', 
+        data: myConfig, 
+        height: '100%', 
+        width: '100%' 
+    });
+}
+/* AJAX request function */
+function request(value) {
+    ourRequest.open('GET','js/test-' + value + '.json');
+    ourRequest.onload = function() {
+        if (ourRequest.status >= 200 && ourRequest.status < 400){
+            words = JSON.parse(ourRequest.responseText);
+            wCloud();
+        }else {
+            console.log("We connected to the server, but it returned an error.");
+        }
+    };
+    ourRequest.onerror = function() {
+        console.log("Connection error");
+    };
+    ourRequest.send();  
+}
+/* Initial AJAX request */
+request(0);
+/* Todays date */
 var today = new Date();
 today.setDate(today.getDate());
-//options for localeToString
 var options = { month: 'numeric', day: 'numeric', year: 'numeric' };
 date.innerHTML = today.toLocaleString('en-US', options);
-// Update the current slider value when moved
+/* Change values based on slider */
 slider.oninput = function () {
+    /* (this.value) day(s) ago */
     output.innerHTML = this.value;
+    /* change date based on how a=many days ago */
     var otherDay = new Date();
     otherDay.setDate(today.getDate() - this.value);
     date.innerHTML = (otherDay.toLocaleString('en-US', options));
+    /* AJAX request based on day */
+    request(this.value);
+//    console.log('js/test-' + this.value + '.json');
+//    console.log(words);
 };
 
 
-var words = [];
-var words = [
-    {"text":"Press","count":"400"},
-    {"text":"word","count":"200"},
-    {"text":"number","count":"200"},
-    {"text":"hello","count":"500"}
-];
-//zingChart
-var myConfig = {
-    "graphset": [ 
-    {
-        "type": "wordcloud",
-  
-         "options": {
-          "style": {
-            "tooltip": {
-              visible: true,
-              text: '%text: %hits'
-            },
-            fontFamily: 'Roboto'
-          },
-            words
-        }
-    }
-    ]
-};
- 
-zingchart.render({ 
-	id: 'myChart', 
-	data: myConfig, 
-	height: '100%', 
-	width: '100%' 
-});
