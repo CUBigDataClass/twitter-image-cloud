0facd0dce4534eb768bb51cda256f0ba162da1ac
angr1252 <47996387+angr1252@users.noreply.github.com>
2019-04-08 20:05:39 -0600

    Add files via upload

--- web/js/script.js
+++ web/js/script.js
@@ -2,52 +2,45 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("sValue");
 output.innerHTML = slider.value;
 
+var today = new Date();
+today.setDate(today.getDate());
+//options for localeToString
+var options = { month: 'numeric', day: 'numeric', year: 'numeric' };
+date.innerHTML = today.toLocaleString('en-US', options);
 // Update the current slider value when moved
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+slider.oninput = function () {
+    output.innerHTML = this.value;
+    var otherDay = new Date();
+    otherDay.setDate(today.getDate() - this.value);
+    date.innerHTML = (otherDay.toLocaleString('en-US', options));
+};
+
+
+var words = [];
+var words = [
+    {"text":"Press","count":"400"},
+    {"text":"word","count":"200"},
+    {"text":"number","count":"200"},
+    {"text":"hello","count":"500"}
+];
 //zingChart
 var myConfig = {
-"graphset":[
-{
-"type":"wordcloud",
-"options":{
-  "style":{
-    "tooltip":{
-      visible: true,
-      text: '%text: %hits'
-    },
-	fontFamily: 'Roboto'
-  },
-"words":[
-{
-"text":"Press",
-"count":"400"
-},
-{
-"text":"USA",
-"count":"300"
-},
-{
-"text":"Waffles",
-"count":"200"
-},
-{
-"text":"hello",
-"count":"150"
-},
-{
-"text":"morning",
-"count":"100"
-},
-{
-"text":"fox",
-"count":"20"
-}
-]
-}
-}
-]
+    "graphset": [ 
+    {
+        "type": "wordcloud",
+  
+         "options": {
+          "style": {
+            "tooltip": {
+              visible: true,
+              text: '%text: %hits'
+            },
+            fontFamily: 'Roboto'
+          },
+            words
+        }
+    }
+    ]
 };
  
 zingchart.render({ 
