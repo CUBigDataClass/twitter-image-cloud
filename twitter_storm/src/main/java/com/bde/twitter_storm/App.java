b7c899678ae044fbdacf60ff82b580924bcdf744
Lucas Dachman <lucas.dachman@gmail.com>
2019-04-13 17:49:05 -0600

    try to change to production mode

--- twitter_storm/src/main/java/com/bde/twitter_storm/App.java
+++ twitter_storm/src/main/java/com/bde/twitter_storm/App.java
@@ -2,6 +2,10 @@ package com.bde.twitter_storm;
 
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.topology.TopologyBuilder;
 
 public class App {
@@ -15,25 +19,33 @@ public class App {
 
         SQLConnect.setup();
 
-        
-        
         Config config = new Config();
         config.setDebug(false);
 
         TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("twitter-spout", new TwitterSpout(consumerKey, consumerSecret, accessToken, accessTokenSecret));
+        builder.setSpout("twitter-spout",
+                new TwitterSpout(consumerKey, consumerSecret, accessToken, accessTokenSecret));
 
-        //bolt to strip tweet down to needed fields
+        // bolt to strip tweet down to needed fields
         builder.setBolt("strip-filter-bolt", new TweetStripBolt()).shuffleGrouping("twitter-spout");
-        //bolt to make NLP api call - this is the current rest point of data
+        // bolt to make NLP api call - this is the current rest point of data
         builder.setBolt("nlp-bolt", new NLPBolt()).shuffleGrouping("strip-filter-bolt");
-        //bolt to make wiki api call
+        // bolt to make wiki api call
         builder.setBolt("wiki-thumb-bolt", new WikiThumbBolt()).shuffleGrouping("nlp-bolt");
-        //bolt to story entity and tweet in SQL db
+        // bolt to story entity and tweet in SQL db
         builder.setBolt("sql-bolt", new SQLBolt()).shuffleGrouping("wiki-thumb-bolt");
 
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("Twitter-to-SQL", config, builder.createTopology());
+        // For local cluster
+        // LocalCluster cluster = new LocalCluster();
+        // cluster.submitTopology("Twitter-to-SQL", config, builder.createTopology());
+
+        config.setNumWorkers(4);
+        config.setMaxSpoutPending(5000);
+        try {
+            StormSubmitter.submitTopology("Awesome_Topology", config, builder.createTopology());
+        } catch (AlreadyAliveException | InvalidTopologyException | AuthorizationException e) {
+            e.printStackTrace();
+        }
 
         
         // try {
